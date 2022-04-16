9ee26d0e8ad7054277b050e05483c8546a7e096b
Safayet-arman <safayetarman11@gmail.com>
2022-04-16 23:39:50 +0600

    Navbar,announcement,categories

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Categories from '../components/Categories'
+
+import Navbar from '../components/Navbar'
+import Slider from '../components/slider'
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Announcement></Announcement>
+        <Navbar/>
+        <Slider/>
+        <Categories/>
+        
+    </div>
+  )
+}
+
+export default Home
